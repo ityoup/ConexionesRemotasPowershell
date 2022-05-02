0f849fe5e328fb7f79f6b4c9bc61eaba1a858efc
ityoup <lizarraga.juan.1gv@gmail.com>
2022-05-02 15:09:43 -0500

    Se actualizo parte del codigo

--- PasarArchivos.ps1
+++ PasarArchivos.ps1
@@ -4,9 +4,9 @@
 #Proximamente tutorial para sacar la informacion de un FTP
 
 
-$Sesion = New-PSSession -ComputerName "CC-D4P"
+$Sesion = New-PSSession -ComputerName "192.168.14.3"
 Copy-Item "C:\Users\jclizarraga\Desktop\Pasodearchivos\ST.exe" -Destination "C:\" -ToSession $Sesion
-Copy-Item "C:\Users\jclizarraga\Desktop\ansible-main" -Destination "C:\" -ToSession $Sesion
+Copy-Item "C:\Users\jclizarraga\Desktop\switches.drawio" -Destination "C:\" -ToSession $Sesion
 Copy-Item "C:\Users\jclizarraga\Desktop\switch.pkt" -Destination "C:\" -ToSession $Sesion
 Copy-Item "C:\Users\jclizarraga\Desktop\instalaciones\ippadGrand Velas\" -Destination "C:\" -ToSession -Recurse $Sesion
 Copy-Item "C:\Users\jclizarraga\Desktop\instalaciones\Cliente\" -Destination "C:\" -ToSession -Recurse $Sesion
@@ -20,8 +20,11 @@ foreach ($comp in $computers)
 
 Invoke-Command -ComputerName "CC-D4P" -ScriptBlock{
 
+choco install tightvnc -ia 'ADDLOCAL=Server SERVER_REGISTER_AS_SERVICE=1 SERVER_ADD_FIREWALL_EXCEPTION=1 SERVER_ALLOW_SAS=1 SET_USEVNCAUTHENTICATION=1 VALUE_OF_USEVNCAUTHENTICATION=1 SET_PASSWORD=1 VALUE_OF_PASSWORD=gvcc20, SET_VIEWONLYPASSWORD=1 VALUE_OF_VIEWONLYPASSWORD=gvcc20, SET_USECONTROLAUTHENTICATION=1 VALUE_OF_USECONTROLAUTHENTICATION=1 SET_CONTROLPASSWORD=1 VALUE_OF_CONTROLPASSWORD=gvcc20,'
 choco install googlechrome
 choco install firefox
+
+#si quieres agregar más, utiliza choco
 
 }
 
