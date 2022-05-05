0e9f260b839e7f6900e5fe924b2b0bd38b545dfa
ityoup <lizarraga.juan.1gv@gmail.com>
2022-05-05 13:51:13 -0500

    Se documentó por medio de comentarios, lo que cada función hace

--- PasarArchivos.ps1
+++ PasarArchivos.ps1
@@ -7,35 +7,38 @@ $Hostname = "CC-S01"
 
 
 #Un método especial para la carpeta de instalación
+
 $Sesion = New-PSSession -ComputerName $Hostname
 Copy-Item "C:\Users\jclizarraga\Desktop\instalaciones\Cliente.zip" -Destination "C:\" -ToSession -Force $Sesion
 
 Invoke-Command -ComputerName $Hostname -ScriptBlock{
 
-Expand-Archive -LiteralPath "C:\Cliente.zip" -DestinationPath "C:\Cliente"
+Expand-Archive -LiteralPath "C:\Cliente.zip" -DestinationPath "C:\"
 Remove-Item -Path "C:\Cliente.zip" -Force
 
 }
 
 #Metodo para la carpeta de Mitrol
+
 Copy-Item "C:\Users\jclizarraga\Desktop\instalaciones\ippadGrand Velas.zip" -Destination "C:\" -ToSession -Recurse $Sesion
 
 Invoke-Command -ComputerName $Hostname -ScriptBlock{
 
-Expand-Archive -LiteralPath "C:\ippadGrand Velas.zip" -DestinationPath "C:\ippad Grand Velas"
+Expand-Archive -LiteralPath "C:\ippadGrand Velas.zip" -DestinationPath "C:\"
 Remove-Item -Path "C:\ippadGrand Velas.zip" -Force
 
 }
 
+#Instalación de Chocolatey
 
 foreach ($comp in $computers)
 {
-   Invoke-Command -computername "CC-D4P" {Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))}
+   Invoke-Command -computername $Hostname {Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))}
 }
 
-Invoke-Command -ComputerName "CC-D4P" -ScriptBlock{
+Invoke-Command -ComputerName $Hostname -ScriptBlock{
 
-choco install tightvnc -ia 'ADDLOCAL=Server SERVER_REGISTER_AS_SERVICE=1 SERVER_ADD_FIREWALL_EXCEPTION=1 SERVER_ALLOW_SAS=1 SET_USEVNCAUTHENTICATION=1 VALUE_OF_USEVNCAUTHENTICATION=1 SET_PASSWORD=1 VALUE_OF_PASSWORD=gvcc20, SET_VIEWONLYPASSWORD=1 VALUE_OF_VIEWONLYPASSWORD=gvcc20, SET_USECONTROLAUTHENTICATION=1 VALUE_OF_USECONTROLAUTHENTICATION=1 SET_CONTROLPASSWORD=1 VALUE_OF_CONTROLPASSWORD=gvcc20,'
+#choco install tightvnc -ia 'ADDLOCAL=Server SERVER_REGISTER_AS_SERVICE=1 SERVER_ADD_FIREWALL_EXCEPTION=1 SERVER_ALLOW_SAS=1 SET_USEVNCAUTHENTICATION=1 VALUE_OF_USEVNCAUTHENTICATION=1 SET_PASSWORD=1 VALUE_OF_PASSWORD=gvcc20, SET_VIEWONLYPASSWORD=1 VALUE_OF_VIEWONLYPASSWORD=gvcc20, SET_USECONTROLAUTHENTICATION=1 VALUE_OF_USECONTROLAUTHENTICATION=1 SET_CONTROLPASSWORD=1 VALUE_OF_CONTROLPASSWORD=gvcc20,'
 choco install googlechrome
 choco install firefox
 
